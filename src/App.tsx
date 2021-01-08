3614a16f1415b98ef1986136f05a216d3afbfb6c
antonymss <demeshkevichanton@gmail.com>
2021-01-08 13:39:25 +0300

    refact

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,19 @@ import './App.css';
 import {Header} from './components/Header/Header';
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
-import {Dialogs, DialogsType} from "./components/Dialogs/Dialogs";
+import {Dialogs} from "./components/Dialogs/Dialogs";
 import {Route} from 'react-router-dom';
 import {News} from './components/News/News';
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
-import {PostsType} from "./components/Profile/MyPosts/MyPosts";
-import {MessageType} from "./components/Dialogs/Message/Message";
-import {addPost} from "./redux/state";
+import {addPost, StateType} from "./redux/state";
 
 export type AppType = {
-    profilePage: profilePageType
-    dialogsPage: dialogsPageType
+    state: StateType
+    addPost:( ) => void
+    updateNewPostText: (newText: string) => void
 }
-export type profilePageType = {
-    posts: Array<PostsType>
-
-}
-
- export type dialogsPageType = {
-    message: Array<MessageType>
-    dialogs: Array<DialogsType>
-}
-
-export type StateType = {
-    state: AppType
-    addPost:(postMessage: string) => void
-}
-
-
-const App = (props: StateType) => {
+const App = (props: AppType) => {
 
     return (
 
@@ -41,7 +24,12 @@ const App = (props: StateType) => {
             <Navbar/>
             <div className='app-wrapper-content'>
                 <Route path='/dialogs' render={() => <Dialogs state={props.state.dialogsPage}/>}/>
-                <Route path='/profile' render={() => <Profile state={props.state.profilePage} addPost={addPost}/>}/>
+                <Route path='/profile' render={() => <Profile
+                    state={props.state.profilePage}
+                    newPostText={props.state.profilePage.newPostText}
+                    addPost={addPost}
+                    updateNewPostText={props.updateNewPostText}
+                />}/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
                 <Route path='/settings' render={() => <Settings/>}/>
