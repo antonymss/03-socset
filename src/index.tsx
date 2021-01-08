45dd0338c481aa741ce2d095382f370b51e0ec31
antonymss <demeshkevichanton@gmail.com>
2021-01-08 13:38:21 +0300

    refact

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
+import {state, subscribe} from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render";
-import {state} from "./redux/state";
+import {addPost, updateNewPostText} from './redux/state'
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
 
 
+export let rerenderEntireTree = () => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+        </BrowserRouter>,
+        document.getElementById('root')
+    );}
 
-rerenderEntireTree(state)
+rerenderEntireTree()
+subscribe(rerenderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,13 +1,43 @@
-import {PostsType} from "../components/Profile/MyPosts/MyPosts";
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () =>{
+    console.log('dgd')
+}
 
-export let  state = {
+export type StateType = {
+    profilePage: ProfilePageType
+    dialogsPage: DialogsPageType
+
+}
+export type DialogsPageType = {
+    dialogs: Array<DialogsType>
+    message: Array<MessageType>
+}
+export type MessageType = {
+    id: number
+    message: string
+}
+export type DialogsType = {
+    id: number
+    name: string
+}
+export type ProfilePageType = {
+    posts: Array<PostsType>
+    newPostText: string
+}
+export type PostsType = {
+    id: number
+    message: string
+    likesCount: number
+}
+
+
+export const state: StateType = {
 
     profilePage: {
         posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 20 },
-            {id: 2, message: 'It\'s my first post?',likesCount: 15 }
+            {id: 1, message: 'Hi, how are you?', likesCount: 20},
+            {id: 2, message: 'It\'s my first post?', likesCount: 15}
         ],
+        newPostText: ''
 
     },
     dialogsPage: {
@@ -19,24 +49,32 @@ export let  state = {
             {id: 5, name: 'Viktor'},
             {id: 6, name: 'Valera'},
         ],
-        message:  [
+        message: [
             {id: 1, message: 'Hi'},
             {id: 2, message: 'How are you?'},
             {id: 3, message: 'YO'},
             {id: 4, message: 'YO'},
             {id: 5, message: 'YO'},]
     }
-    }
+}
 
 
-
-
-    export let addPost = (postMessage: string) => {
+export const addPost = () => {
     let newPost: PostsType = {
-        id:5,
-        message: postMessage,
+        id: 5,
+        message: state.profilePage.newPostText,
         likesCount: 0
     }
-        state.profilePage.posts.push(newPost)
-        rerenderEntireTree(state)
-    }
+    state.profilePage.posts.push(newPost)
+    state.profilePage.newPostText = ''
+    rerenderEntireTree()
+}
+
+export const updateNewPostText = (newText: string) => {
+    state.profilePage.newPostText = newText
+    rerenderEntireTree()
+}
+
+export const subscribe = (observer: () => void) => {
+    rerenderEntireTree = observer
+}
