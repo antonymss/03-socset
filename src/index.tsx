b87e2903a56e2c5dbdb3d798fca20f8dc1067c8f
antonymss <demeshkevichanton@gmail.com>
2021-01-14 17:14:58 +0300

    refact

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ let rerenderEntireTree = (state: StateType) => {
     debugger
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} store={store}  addPost={store.addPost.bind(store)}
+            <App store={store} state={state} addPost={store.addPost.bind(store)}
                  updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>, document.getElementById('root'));
 }
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -82,6 +82,26 @@ let store: StoreType = {
     },
     getState() {
         return this._state
+    },
+    dispatch(action){
+        if (action.type === 'ADD-POST') {
+            {
+                let newPost: PostsType = {
+                    id: 5,
+                    message: this._state.profilePage.newPostText,
+                    likesCount: 0
+                }
+                this._state.profilePage.posts.push(newPost)
+                this._state.profilePage.newPostText = ''
+                this._callSubscriber(this._state)
+            }
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT'){
+            {
+                this._state.profilePage.newPostText = action.newText
+                this._callSubscriber(this._state)
+            }
+        }
+
     }
 }
 
