72bff9c600d44472a167a25a7f2756554125d369
antonymss <demeshkevichanton@gmail.com>
2021-01-17 10:41:42 +0300

    refact

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './Dialogs.module.css'
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
-import {DialogsPageType} from "../../redux/state";
+import {sendMessageCreator, StoreType, updateNewMessageBodyCreator} from "../../redux/state";
 
 
 type DialogType = {
-    dialogsPage: DialogsPageType
+
+    store: StoreType
 }
-
-
 
 
 export const Dialogs: React.FC<DialogType> = (props) => {
 
-    let dialogsElements = props.dialogsPage.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let dialogsElements = props.store._state.dialogsPage.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
 
-let messagesElements = props.dialogsPage.message.map(m => <Message message={m.message} id={m.id}/>)
+    let messagesElements = props.store._state.dialogsPage.message.map(m => <Message message={m.message} id={m.id}/>)
+    let newMessageBody = props.store._state.dialogsPage.newMessageBody
+    let onSendMessageClick = () => {
+        props.store.dispatch(sendMessageCreator())
+    }
+let onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>)=>{
+     let body = e.target.value
+    props.store.dispatch(updateNewMessageBodyCreator(body))
+}
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                {dialogsElements}
+            </div>
 
+            <div className={s.messages}>
+                <div> {messagesElements}</div>
+                <div>
+                    <div><textarea value={newMessageBody} onChange={onNewMessageChange} placeholder='Enter your message'></textarea></div>
+                    <div>
+                        <button onClick={onSendMessageClick}> Add post
+                        </button>
+                    </div>
+                </div>
+            </div>
 
-return (
-    <div className={s.dialogs}>
-        <div className={s.dialogsItems}>
-            {dialogsElements}
         </div>
-
-        <div className={s.messages}>
-            {messagesElements}
-        </div>
-        <div>
-            <textarea></textarea>
-        </div>
-        <div>
-            <button> Add post</button>
-        </div>
-    </div>
-)
+    )
 }
 
