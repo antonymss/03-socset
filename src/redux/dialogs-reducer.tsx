8f9c1df4d79fe4f64904df98de4c56b37afee2d0
antonymss <demeshkevichanton@gmail.com>
2021-01-24 18:14:05 +0300

    refact

--- src/redux/dialogs-reducer.tsx
+++ src/redux/dialogs-reducer.tsx
@@ -1,11 +1,29 @@
-import {ActionType, DialogsPageType} from "./state";
+import {ActionType, DialogsPageType} from "./store";
 import {ChangeEvent} from "react";
 
 
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
-export const dialogsReducer = (state: DialogsPageType, action: ActionType) => {
+let initialState = {
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrew'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Alex'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Valera'},
+        ],
+        message: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How are you?'},
+            {id: 3, message: 'YO'},
+            {id: 4, message: 'YO'},
+            {id: 5, message: 'YO'}],
+        newMessageBody: ''
+    }
+
+export const dialogsReducer = (state: DialogsPageType = initialState, action: ActionType) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body
