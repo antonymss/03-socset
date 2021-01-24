4ca2bbfb3808c939803d9186d3242ac5c28eddb4
antonymss <demeshkevichanton@gmail.com>
2021-01-24 18:13:52 +0300

    refact

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,30 +2,30 @@ import React, {ChangeEvent} from 'react';
 import s from './Dialogs.module.css'
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
-import {StoreType} from "../../redux/state";
-import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-teducer";
+import {DialogsPageType} from "../../redux/store";
 
 
 type DialogType = {
-
-    store: StoreType
+    dialogsPage: DialogsPageType
+    updateNewMessageBody: (e:ChangeEvent<HTMLTextAreaElement>) => void
+    sendMessage: () => void
 }
 
 
 export const Dialogs: React.FC<DialogType> = (props) => {
 
-    let state = props.store.getState().dialogsPage
+    let state = props.dialogsPage
 
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
     let messagesElements = state.message.map(m => <Message message={m.message} id={m.id}/>)
     let newMessageBody = state.newMessageBody
     let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator())
+        props.sendMessage()
     }
-let onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>)=>{
-     let body = e.currentTarget.value
-    props.store.dispatch(updateNewMessageBodyCreator(e))
-}
+    let onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+
+        props.updateNewMessageBody(e)
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -35,9 +35,10 @@ let onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>)=>{
             <div className={s.messages}>
                 <div> {messagesElements}</div>
                 <div>
-                    <div><textarea value={newMessageBody} onChange={onNewMessageChange} placeholder='Enter your message'></textarea></div>
+                    <div><textarea value={newMessageBody} onChange={onNewMessageChange}
+                                   placeholder='Enter your message'></textarea></div>
                     <div>
-                        <button onClick={onSendMessageClick}> Add post
+                        <button  onClick={onSendMessageClick}> Add post
                         </button>
                     </div>
                 </div>
