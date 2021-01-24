edc9fb368e0054d46032c35be27ab52f9a7e99cf
antonymss <demeshkevichanton@gmail.com>
2021-01-24 18:13:39 +0300

    refact

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import s from './DialogItem.module.css'
-import {DialogsType} from "../../../redux/state";
+import {DialogsType} from "../../../redux/store";
 
 // type DialogItemType = {
 //     name: string
