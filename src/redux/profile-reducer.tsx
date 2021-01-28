2f7210631961b14fc803c91758e2bc88a2b84c03
antonymss <demeshkevichanton@gmail.com>
2021-01-28 12:00:05 +0300

    refact

--- src/redux/profile-reducer.tsx
+++ src/redux/profile-reducer.tsx
@@ -1,10 +1,18 @@
-import {ActionType, PostsType, ProfilePageType} from "./state";
+import {ActionType, PostsType, ProfilePageType} from "./store";
 import {ChangeEvent} from "react";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-export const profileReducer = (state: ProfilePageType, action: ActionType) => {
+let initialState = {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: 20},
+            {id: 2, message: 'It\'s my first post?', likesCount: 15}
+        ],
+        newPostText: ''
+}
+
+export const profileReducer = (state: ProfilePageType = initialState, action: ActionType) => {
     switch (action.type) {
         case ADD_POST:
             let newPost: PostsType = {
