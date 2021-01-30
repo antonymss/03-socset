32ab7e6ade4562ea5c8236bf6feb069828c410af
antonymss <demeshkevichanton@gmail.com>
2021-01-30 12:11:17 +0300

    refact

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {Settings} from "./components/Settings/Settings";
 import {ActionType, StateType} from "./redux/store";
 import  {StoreReduxType} from "./redux/redux-store";
 import { DialogsContainer } from './components/Dialogs/DialogsContainer';
+import { UsersContainer } from './components/Users/UsersContainer';
 
 
 export type AppType = {
@@ -30,7 +31,7 @@ const App = () => {
                     <Route path='/dialogs' render={() => <DialogsContainer  />}/>
                     <Route path='/profile' render={() => <Profile
                 />}/>
-                <Route path='/users' render={() => <div>users</div> }/>
+                <Route path='/users' render={() => <UsersContainer/> }/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
                 <Route path='/settings' render={() => <Settings/>}/>
