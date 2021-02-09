1629934ce0f608c17b18342ec337b66b95b73530
antonymss <demeshkevichanton@gmail.com>
2021-02-09 16:01:35 +0300

    refact

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './users.module.css';
-import { userType} from "../../redux/users-reducer";
-
-
+import {userType} from "../../redux/users-reducer";
+import axios from "axios";
+import userPhoto from '../../assets/images/user.png'
 
 type UsersComponentType = {
     users: userType []
@@ -12,55 +12,38 @@ type UsersComponentType = {
 }
 
 export let Users = (props: UsersComponentType) => {
-if (props.users.length === 0){
-    props.setUsers([
-        {
-            id: 1,
-            photoUrl: 'https://s1.1zoom.ru/big3/436/Salvador_Dali_Closeup_Men_Face_Black_and_white_536188_3030x3460.jpg',
-            followed: false,
-            fullName: 'Dmitry',
-            status: 'I am fine',
-            location: {city: 'Minsk', country: 'Belarus'}
-        },
-        {
-            id: 2,
-            photoUrl: 'https://s1.1zoom.ru/big3/436/Salvador_Dali_Closeup_Men_Face_Black_and_white_536188_3030x3460.jpg',
-            followed: true,
-            fullName: 'Sasha',
-            status: 'I am fine',
-            location: {city: 'Moscow', country: 'Russia'}
-        },
-        {
-            id: 3,
-            photoUrl: 'https://s1.1zoom.ru/big3/436/Salvador_Dali_Closeup_Men_Face_Black_and_white_536188_3030x3460.jpg',
-            followed: false,
-            fullName: 'Andrew',
-            status: 'I am fine',
-            location: {city: 'Kiev', country: 'Ukraine'}
-        }
-    ])}
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response =>{
+            props.setUsers(response.data.items)
+        })
+
+    }
 
     return <div>
         {
             props.users.map(u => <div key={u.id}>
               <span>
                   <div>
-                     <img src={u.photoUrl} className={styles.userPhoto}/>
+                     <img src={u.photos.small !==null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                   </div>
                   <div>
                       {u.followed
-                          ? <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>
-                          : <button onClick={()=>{props.follow(u.id)}}>Follow</button>}
+                          ? <button onClick={() => {
+                              props.unfollow(u.id)
+                          }}>Unfollow</button>
+                          : <button onClick={() => {
+                              props.follow(u.id)
+                          }}>Follow</button>}
 
                   </div>
               </span>
                 <span>
                    <span>
-                       <div>{u.fullName}</div><div>{u.status}</div>
+                       <div>{u.name}</div><div>{u.status}</div>
                    </span>
                    <span>
-                       <div>{u.location.country}</div>
-                       <div>{u.location.city}</div>
+                       <div>{'u.location.country'}</div>
+                       <div>{'u.location.city'}</div>
                    </span>
                </span>
             </div>)
