789788246d282023cc3a94c9e301d5b0616bc8aa
antonymss <demeshkevichanton@gmail.com>
2021-02-17 16:17:45 +0300

    refact

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import './App.css';
 import {Header} from './components/Header/Header';
 import {Navbar} from "./components/Navbar/Navbar";
-import {Profile} from "./components/Profile/Profile";
 import {Route} from 'react-router-dom';
 import {News} from './components/News/News';
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 import {ActionType, StateType} from "./redux/store";
-import  {StoreReduxType} from "./redux/redux-store";
-import { DialogsContainer } from './components/Dialogs/DialogsContainer';
-import { UsersContainer } from './components/Users/UsersContainer';
+import {StoreReduxType} from "./redux/redux-store";
+import {DialogsContainer} from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
 
 
 export type AppType = {
     store: StoreReduxType
     state: StateType
-    dispatch: (action:ActionType)=>void
+    dispatch: (action: ActionType) => void
 
 }
 const App = () => {
@@ -28,10 +28,9 @@ const App = () => {
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                    <Route path='/dialogs' render={() => <DialogsContainer  />}/>
-                    <Route path='/profile' render={() => <Profile
-                />}/>
-                <Route path='/users' render={() => <UsersContainer/> }/>
+                <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                <Route path='/users' render={() => <UsersContainer/>}/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
                 <Route path='/settings' render={() => <Settings/>}/>
