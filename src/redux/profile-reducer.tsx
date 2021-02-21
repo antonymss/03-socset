226b697405a60cb443cee699f6f15be37b61df73
antonymss <demeshkevichanton@gmail.com>
2021-02-21 10:29:42 +0300

    refact

--- src/redux/profile-reducer.tsx
+++ src/redux/profile-reducer.tsx
@@ -1,40 +1,80 @@
-import {ActionType, PostsType, ProfilePageType} from "./store";
+import {ActionType} from "./store";
 import {ChangeEvent} from "react";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
-let initialState = {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 20},
-            {id: 2, message: 'It\'s my first post?', likesCount: 15}
-        ],
-        newPostText: ''
+
+export type PostType = {
+    id: number
+    message: string
+    likesCount: number
 }
 
-export const profileReducer = (state: ProfilePageType = initialState, action: ActionType) => {
+export type ProfileType = {
+    userId: number
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    fullName: string
+    contacts:
+        {
+            github: string
+            vk: string
+            facebook: string
+            instagram: string
+            twitter: string
+            website: string
+            youtube: string
+            mainLink: string
+        }
+    photos: {
+    small: string
+    large: string
+}
+}
+
+
+let initialState = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 20},
+        {id: 2, message: 'It\'s my first post?', likesCount: 15}
+    ] as Array<PostType>,
+    newPostText: '',
+    profile: null as ProfileType | null
+}
+
+export type InitialProfileStateType = typeof initialState
+
+export const profileReducer = (state: InitialProfileStateType = initialState, action: ActionType): InitialProfileStateType => {
     switch (action.type) {
         case ADD_POST:
-            let newPost: PostsType = {
+            let newPost: PostType = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             }
-            return  {
+            return {
                 ...state,
                 posts: [...state.posts, newPost],
-                newPostText : ''
+                newPostText: ''
             }
         case UPDATE_NEW_POST_TEXT:
-            return  {
+            return {
                 ...state,
                 newPostText: action.newText
+            }
+        case SET_USER_PROFILE:
+            return {
+                ...state,
+                profile: action.profile
             }
         default:
             return state
     }
 }
-export let addPostActionCreator = () => ({type: ADD_POST}) as const
+export let addPostActionCreator = () => ({type: ADD_POST} as const)
+export let setUserProfile = (profile: ProfileType) => ({type: SET_USER_PROFILE, profile} as const)
 export let updateNewPostTextActionCreator = (e: ChangeEvent<HTMLTextAreaElement>) => {
     return {type: UPDATE_NEW_POST_TEXT, newText: e.currentTarget.value} as const
 }
