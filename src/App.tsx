c7f3e632e5cf76f500b484e06a9e57c0b200259f
antonymss <demeshkevichanton@gmail.com>
2021-02-23 17:18:16 +0300

    refact

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import {Header} from './components/Header/Header';
 import {Navbar} from "./components/Navbar/Navbar";
 import {Route} from 'react-router-dom';
 import {News} from './components/News/News';
@@ -11,6 +10,8 @@ import {StoreReduxType} from "./redux/redux-store";
 import {DialogsContainer} from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
+
 
 
 export type AppType = {
@@ -25,7 +26,7 @@ const App = () => {
     return (
 
         <div className='app-wrapper'>
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className='app-wrapper-content'>
                 <Route path='/dialogs' render={() => <DialogsContainer/>}/>
