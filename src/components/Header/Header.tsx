c3ae500c85fb3b719b71df87069ca796b1deaba9
antonymss <demeshkevichanton@gmail.com>
2021-02-24 14:58:54 +0300

    refact

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import s from './Header.module.css';
 
-export const Header = () => {
-    return  <header className ={s.header}>
-        <img src='https://yt3.ggpht.com/ytc/AAUvwnhcOYdTa8LLrCrXBvmGL2kd1pP1xiGLChGUaz4KGA=s48-c-k-c0xffffffff-no-rj-mo'/>
+import {HeaderContainerType} from "./HeaderContainer";
+
+export const Header = (props: HeaderContainerType ) => {
+    return <header className={s.header}>
+        <img
+            src='https://yt3.ggpht.com/ytc/AAUvwnhcOYdTa8LLrCrXBvmGL2kd1pP1xiGLChGUaz4KGA=s48-c-k-c0xffffffff-no-rj-mo'/>
+
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login :
+                <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 }
 
