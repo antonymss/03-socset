9d420bd4a31160cc1eb0112163902faaa8811823
antonymss <demeshkevichanton@gmail.com>
2021-03-07 15:18:52 +0300

    refact

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -4,12 +4,14 @@ import {Dialogs} from "./Dialogs";
 import {connect} from "react-redux";
 import {Dispatch} from "redux";
 import {AppStateType} from "../../redux/redux-store";
+import {Redirect} from "react-router-dom";
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 
 
 let mapStateToProps = (state:AppStateType)=>{
     return{
         dialogsPage: state.dialogsPage,
-        isAuth: state.auth.isAuth
+
     }
 }
 let mapDispatchToProps = (dispatch:Dispatch)=>{
@@ -19,4 +21,7 @@ return{
 }
 }
 
-export const DialogsContainer = connect(mapStateToProps,mapDispatchToProps)(Dialogs)
+let AuthRedirectComponent = withAuthRedirect(Dialogs)
+
+
+export const DialogsContainer = connect(mapStateToProps,mapDispatchToProps)(AuthRedirectComponent)
