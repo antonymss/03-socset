135e9bf2e7dd40c9cbdf9b0b394a0dce1e5c5e97
antonymss <demeshkevichanton@gmail.com>
2021-03-10 14:14:35 +0300

    refact

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -2,26 +2,32 @@ import React from 'react';
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 import {Dialogs} from "./Dialogs";
 import {connect} from "react-redux";
-import {Dispatch} from "redux";
+import {compose, Dispatch} from "redux";
 import {AppStateType} from "../../redux/redux-store";
-import {Redirect} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 
 
-let mapStateToProps = (state:AppStateType)=>{
-    return{
+let mapStateToProps = (state: AppStateType) => {
+    return {
         dialogsPage: state.dialogsPage,
 
     }
 }
-let mapDispatchToProps = (dispatch:Dispatch)=>{
-return{
-    updateNewMessageBody:(text: string)=>{dispatch(updateNewMessageBodyCreator(text))},
-    sendMessage:()=>{dispatch(sendMessageCreator())}
+let mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        updateNewMessageBody: (text: string) => {
+            dispatch(updateNewMessageBodyCreator(text))
+        },
+        sendMessage: () => {
+            dispatch(sendMessageCreator())
+        }
+    }
 }
-}
 
-let AuthRedirectComponent = withAuthRedirect(Dialogs)
+export const DialogsContainer = compose<React.ComponentType>(connect(mapStateToProps, mapDispatchToProps), withAuthRedirect)
+(Dialogs)
 
-
-export const DialogsContainer = connect(mapStateToProps,mapDispatchToProps)(AuthRedirectComponent)
+// let AuthRedirectComponent = withAuthRedirect(Dialogs)
+//
+//
+// export const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(AuthRedirectComponent)
