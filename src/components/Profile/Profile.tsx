8d909460cc9bc12eda7f6eecccc0adb6db27a647
antonymss <demeshkevichanton@gmail.com>
2021-03-11 13:05:02 +0300

    refact

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -6,12 +6,14 @@ import {ProfileType} from "../../redux/profile-reducer";
 
 type PropsType = {
     profile: ProfileType | null
+    status: string
+    updateStatus: string
 }
 
 export const Profile = (props: PropsType) => {
     debugger
     return <div>
-        <ProfileInfo profile={props.profile}/>
+        <ProfileInfo profile={props.profile} status={props.status} updateStatus={props.updateStatus}/>
         <MyPostContainer/>
 
     </div>
