f509d2cf1e0b9da2e032126b7dc98f90e6fb875c
antonymss <demeshkevichanton@gmail.com>
2021-03-17 13:06:01 +0300

    refact

---
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './FormsControls.module.css'
+
+const FormControl = ({input, meta,child, ...props}: any)=>{
+    const hasError = meta.touched && meta.error
+    return (
+        <div className={hasError ? styles.error : ''}>
+            <div>
+                {props.children}
+            </div>
+            {hasError && <span>{meta.error}</span>}
+        </div>
+    )
+}
+
+export const Textarea  = (props:any) => {
+    const {input, meta,child, ...restProps} = props
+    return <FormControl {...props}><textarea {...input} {...restProps} /></FormControl>
+}
+export const Input  = (props:any) => {
+    const {input, meta,child, ...restProps} = props
+    return <FormControl {...props}><input {...input} {...restProps} /></FormControl>
+}
+
+// export const Input  = ({input, meta, ...props}:WrappedFieldProps) => {
+//
+//     const hasError = meta.touched && meta.error
+//
+//     return (
+//         <div className={styles.formControl + '' + (hasError ? styles.error : '')}>
+//             <div>
+//                 <input {...input} {...props} />
+//             </div>
+//             {hasError && <span>{meta.error}</span>}
+//         </div>
+//     )
+// }
