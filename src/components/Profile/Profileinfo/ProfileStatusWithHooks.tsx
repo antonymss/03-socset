592a428f018b51e281fe0444ce1d0e778d1774a0
antonymss <demeshkevichanton@gmail.com>
2021-04-01 11:25:03 +0300

    refact

---
+++ src/components/Profile/Profileinfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,44 @@
+import React, {ChangeEvent, useEffect, useState} from 'react';
+
+type ProfileStatusType = {
+    status: string
+    updateStatus: any
+
+}
+
+export const ProfileStatusWithHooks = (props: ProfileStatusType) => {
+
+    let [editMode, setEditMode] = useState(false)
+    let [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        setStatus(props.status)
+    },[props.status])
+
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
+   const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+    const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value)
+    }
+    return (
+        <div>
+            {!editMode &&
+            <div>
+                <span onDoubleClick={activateEditMode}>{props.status || '----'} </span>
+            </div>
+            }
+            {editMode &&
+            <div>
+                <input autoFocus={true} onBlur={deactivateEditMode} onChange={onStatusChange} value={status}
+                />
+            </div>
+            }
+        </div>
+    )
+
+}
