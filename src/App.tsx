0bdaa6c1289470734efa07718fad208a8cd05cb0
antonymss <demeshkevichanton@gmail.com>
2021-04-11 13:33:58 +0300

    refact

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import HeaderContainer from './components/Header/HeaderContainer';
 import UsersContainer from "./components/Users/UsersContainer";
 import Login from './components/Login/Login';
 import {connect} from "react-redux";
-import {getAuthUserData} from "./redux/auth-reducer";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import {AppStateType} from "./redux/redux-store";
