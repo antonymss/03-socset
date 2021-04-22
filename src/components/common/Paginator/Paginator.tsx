3bf37301a142b6a43887513e69bc31e542c722b8
antonymss <demeshkevichanton@gmail.com>
2021-04-22 12:39:45 +0300

    refact

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,31 +1,110 @@
-import React from 'react';
-import styles from "./Paginator.module.css";
+import React, {useState} from 'react'
+import styles from './Paginator.module.css'
+import cn from 'classnames'
+// classname библеотека позволяет писать неограниченное количество классов в ClassName через запятую.
 
-type PaginatorType = {
-    currentPage: number
+
+type PropsType = {
+    totalItemsCount: number
     pageSize: number
-    totalUsersCount: number
-    onPageChanged: any
-
+    currentPage?: number
+    onPageChanged?: (pageNumber: number) => void
+    portionSize?: number
 }
 
-export let Paginator = (props: PaginatorType) => {
+export const Paginator: React.FC<PropsType> = ({
+                                                   totalItemsCount, pageSize,
+                                                   currentPage = 1,
+                                                   onPageChanged = x => x,
+                                                   portionSize = 10
+                                               }) => {
 
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
-    let pages = []
+    let pages: Array<number> = [];
     for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
+        pages.push(i);
     }
 
-    return    <div>
-        {pages.map(p => {
-            return <span className={props.currentPage === p ? styles.selectedPage : ''}
-                         onClick={() => {
-                             debugger
-                             props.onPageChanged(p)
-                         }}>{p}</span>
-        })}
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
+
+
+    return <div className={cn(styles.paginator)}>
+        {portionNumber > 1 &&
+        <button onClick={() => {
+            setPortionNumber(portionNumber - 1)
+        }}>PREV</button>}
+
+        {pages
+            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map((p) => {
+                return <span className={cn({
+                    [styles.selectedPage]: currentPage === p
+                }, styles.pageNumber)}
+                             key={p}
+                             onClick={(e) => {
+                                 onPageChanged(p);
+                             }}>{p}</span>
+            })}
+        {portionCount > portionNumber &&
+        <button onClick={() => {
+            setPortionNumber(portionNumber + 1)
+        }}>NEXT</button>}
+
 
     </div>
 }
+
+// import React, {useState} from 'react';
+// import styles from "./Paginator.module.css";
+// import cn from "classnames"
+//
+// type PaginatorType = {
+//     currentPage: number
+//     pageSize: number
+//     totalItemsCount: number
+//     onPageChanged: (pageNumber:number)=> void
+//portionSize?: number
+//
+// }
+//
+// export let Paginator = (props: PaginatorType, portionSize = 10) => {
+//
+//     debugger
+//     let pagesCount = Math.ceil(props.totalItemsCount / props.pageSize)
+//
+//     let pages = []
+//     for (let i = 1; i <= pagesCount; i++) {
+//         pages.push(i)
+//     }
+//     let portionCount = Math.ceil(pagesCount / portionSize)
+//     let [portionNumber, setPortionNumber] = useState(1)
+//     let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+//     let rightPortionPageNumber = portionNumber * portionSize
+//
+//     return <div className={styles.paginator}>
+//         {portionNumber > 1 &&
+//         <button onClick={() => {
+//             setPortionNumber(portionNumber - 1)
+//         }}>PREV</button>}
+//
+//         {pages.filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+//             .map((p) => {
+//                 return <span className={cn({
+//                     [styles.selectedPage]: props.currentPage === p
+//                 }, styles.pageNumber)}
+//                              key={p}
+//                              onClick={() => {
+//                                  props.onPageChanged(p)
+//                              }}>{p}</span>
+//             })}
+//         {portionCount > portionNumber && <button onClick={() => {
+//             setPortionNumber(portionNumber + 1)
+//         }}>Next</button>}
+//
+//     </div>
+// }
